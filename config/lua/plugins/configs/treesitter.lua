5a0e2382a898ac67b32425e046c9cfdfaa3597e2
Foo Yong Qi <yongqifoo@gmail.com>
2024-05-17 12:16:59 +0800

    Initial Commit

---
+++ config/lua/plugins/configs/treesitter.lua
@@ -0,0 +1,12 @@
+local options = {
+  ensure_installed = { "lua", "vim", "vimdoc", "haskell", "python", "java", "markdown" },
+
+  highlight = {
+    enable = true,
+    use_languagetree = true,
+  },
+
+  indent = { enable = true },
+}
+
+return options
